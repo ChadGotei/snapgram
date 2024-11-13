cba28aa5ec75749e4e2ba4752a1674af779c890d
ChadGotei <memerdank199@gmail.com>
2024-11-13 17:51:28 +0530

    Initial commit

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -349,28 +349,27 @@ export async function deletePost(postId: string, imageId: string) {
     }
 }
 
-export async function getInfinitePosts({pageParam}: {pageParam: number}) {
-    const queries = [Query.orderDesc('$updatedAt'), Query.limit(10)]
-
-    if(pageParam) {
-        queries.push(Query.cursorAfter(pageParam.toString()));
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+    const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(9)];
+  
+    if (pageParam) {
+      queries.push(Query.cursorAfter(pageParam.toString()));
     }
-
+  
     try {
-        const posts = await databases.listDocuments(
-            appwriteConfig.databaseId,
-            appwriteConfig.postCollectionId,
-            queries
-        )
-
-        if(!posts) throw Error;
-
-        return posts;
-    } catch(error) {
-        console.log(error);
+      const posts = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        queries
+      );
+  
+      if (!posts) throw Error;
+  
+      return posts;
+    } catch (error) {
+      console.log(error);
     }
-}
-
+  }
 export async function searchPost({searchItem}:{ searchItem: string}) {
     try {
         const posts = await databases.listDocuments(
