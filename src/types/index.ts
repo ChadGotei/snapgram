4c80c127d45e5afc99145e06140fa0fa54ea252a
ChadGotei <memerdank199@gmail.com>
2024-11-13 18:02:38 +0530

    Intial commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,57 +1,76 @@
+// Types for Post and Pagination
+
+export type Post = {
+  $id: string;
+  caption: string;
+  userId: string;
+  imageUrl: string;
+  location?: string;
+  tags?: string;
+  createdAt: string;
+  updatedAt: string;
+  // add other post properties as needed
+}
+
+export type PostsResponse = {
+  documents: Post[];
+  total?: number; // Optional, if the API returns a total count of posts
+}
+
 export type IContextType = {
   user: IUser;
   isLoading: boolean;
   setUser: React.Dispatch<React.SetStateAction<IUser>>;
-  isAuthenticated: boolean,
+  isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
   checkAuthUser: () => Promise<boolean>;
-}
+};
 
 export type INavLink = {
-    imgURL: string;
-    route: string;
-    label: string;
-  };
-  
-  export type IUpdateUser = {
-    userId: string;
-    name: string;
-    bio: string;
-    imageId: string;
-    imageUrl: URL | string;
-    file: File[];
-  };
-  
-  export type INewPost = {
-    userId: string;
-    caption: string;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
-  
-  export type IUpdatePost = {
-    postId: string;
-    caption: string;
-    imageId: string;
-    imageUrl: URL;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
-  
-  export type IUser = {
-    id: string;
-    name: string;
-    username: string;
-    email: string;
-    imageUrl: string;
-    bio: string;
-  };
-  
-  export type INewUser = {
-    name: string;
-    email: string;
-    username: string;
-    password: string;
-  };
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
